60d7a172c5d1e3c10fab486207c2b616fbee2b80
shashiweb <shashiweb64@gmail.com>
2024-07-08 14:11:49 +0530

    change

--- src/About.js
+++ src/About.js
@@ -32,6 +32,8 @@ export function Myabout1()
         <img src={myimage2} width="300" alt="photos10"/>
         <div className="first">this is text <h1>heading</h1></div>
         {10+90}
+        <input type="button" value="submit" className="btn btn-danger shadow p-3"/>
+
         </div>
         </Fragment>
     )
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 import Xys from './About';
 import { Myabout1 } from './About';
+
 import "./style.css"; 
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
